c869e4f77c0a3bbdf85cc5bfe9bcea2e730c737e
Igor Sidorov <isidorov@wiley.com>
2024-07-21 11:21:01 +0300

    w

--- calc/src/Main.java
+++ calc/src/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
         int a = calc.plus.apply(1, 2);
         int b = calc.minus.apply(1,1);
-        int c = calc.devide.apply(a, b);
+        int c = calc.divide.apply(a, b);
 //      Ошибка возникает, так как на 0 делить нельзя.
 //      Можно поставить условие, если y !=0 и возращать другое значение например 0
         calc.println.accept(c);
