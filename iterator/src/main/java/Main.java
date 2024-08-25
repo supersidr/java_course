5e2fd5250c4baf14d9a3fd2f0c2eabaf2e6cc6af
Igor Sidorov <isidorov@wiley.com>
2024-08-25 09:54:14 +0300

    wip

--- iterator/src/main/java/Main.java
+++ iterator/src/main/java/Main.java
@@ -1,2 +1,11 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+        for (int r : new Randoms(90, 100)) {
+            System.out.println("Случайное число: " + r);
+            if (r == 100) {
+                System.out.println("Выпало число 100, давайте на этом закончим");
+                break;
+            }
+        }
+    }
 }
--- iterator/src/main/java/Randoms.java
+++ iterator/src/main/java/Randoms.java
@@ -1,2 +1,29 @@
-package PACKAGE_NAME;public class Randoms {
+import java.util.Iterator;
+import java.util.Random;
+
+public class Randoms implements Iterable<Integer> {
+    protected Random random;
+    protected int min;
+    protected int max;
+
+    public Randoms(int min, int max) {
+        this.min = min;
+        this.max = max;
+        this.random = new Random();
+    }
+
+    @Override
+    public Iterator<Integer> iterator() {
+        return new Iterator<Integer>() {
+            @Override
+            public boolean hasNext() {
+                return true;
+            }
+
+            @Override
+            public Integer next() {
+                return random.nextInt(max - min + 1) + min;
+            }
+        };
+    }
 }
