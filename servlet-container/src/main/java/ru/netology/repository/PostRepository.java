485ec5a7b33e870f328bc1a460530739d11724ee
Igor Sidorov <isidorov@wiley.com>
2024-11-09 12:33:11 +0300

    wip

--- servlet-container/src/main/java/ru/netology/repository/PostRepository.java
+++ servlet-container/src/main/java/ru/netology/repository/PostRepository.java
@@ -11,38 +11,38 @@ import java.util.concurrent.atomic.AtomicLong;
 
 // Stub
 public class PostRepository {
-  private final ConcurrentHashMap<Long, Post> posts;
-  private final AtomicLong idGenerator = new AtomicLong();
+    private final ConcurrentHashMap<Long, Post> posts;
+    private final AtomicLong idGenerator = new AtomicLong();
 
-  public PostRepository() {
-    posts = new ConcurrentHashMap<>();
-  }
-
-  public List<Post> all() {
-    return new ArrayList<>(posts.values());
-  }
-
-  public Optional<Post> getById(long id) {
-    return Optional.ofNullable(posts.get(id));
-  }
-
-  public Post save(Post post) {
-    if (post.getId() != 0 && !posts.containsKey(post.getId())) {
-      throw new NotFoundException();
+    public PostRepository() {
+        posts = new ConcurrentHashMap<>();
     }
 
-    if (post.getId() == 0) {
-      var newId = idGenerator.incrementAndGet();
-      post.setId(newId);
-      posts.put(newId, post);
+    public List<Post> all() {
+        return new ArrayList<>(posts.values());
     }
 
-    posts.put(post.getId(), post);
+    public Optional<Post> getById(long id) {
+        return Optional.ofNullable(posts.get(id));
+    }
 
-    return post;
-  }
+    public Post save(Post post) {
+        if (post.getId() != 0 && !posts.containsKey(post.getId())) {
+            throw new NotFoundException();
+        }
 
-  public void removeById(long id) {
-      posts.containsKey(id) ? posts.remove(id) : throw new NotFoundException();
-  }
+        if (post.getId() == 0) {
+            var newId = idGenerator.incrementAndGet();
+            post.setId(newId);
+            posts.put(newId, post);
+        }
+
+        posts.put(post.getId(), post);
+
+        return post;
+    }
+
+    public void removeById(long id) {
+        posts.containsKey(id) ? posts.remove(id) : throw new NotFoundException();
+    }
 }
