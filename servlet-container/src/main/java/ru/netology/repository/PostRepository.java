a4518b09f5a64643d3e02e76dc88d779f3802c6e
Igor Sidorov <isidorov@wiley.com>
2024-11-18 19:00:30 +0300

    wip

--- servlet-container/src/main/java/ru/netology/repository/PostRepository.java
+++ servlet-container/src/main/java/ru/netology/repository/PostRepository.java
@@ -1,5 +1,6 @@
 package ru.netology.repository;
 
+import org.springframework.stereotype.Repository;
 import ru.netology.exception.NotFoundException;
 import ru.netology.model.Post;
 
@@ -9,6 +10,7 @@ import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicLong;
 
+@Repository
 // Stub
 public class PostRepository {
     private final ConcurrentHashMap<Long, Post> posts;
@@ -43,6 +45,7 @@ public class PostRepository {
     }
 
     public void removeById(long id) {
-        posts.containsKey(id) ? posts.remove(id) : throw new NotFoundException();
+        posts.remove(id);
+        throw new NotFoundException();
     }
 }
